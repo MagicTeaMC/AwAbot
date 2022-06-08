ca580f7e9ae72e1b8533114545aaaed19866cbdb
MagicTeaMC <95519633+MagicTeaMC@users.noreply.github.com>
2022-06-08 10:23:00 +0800

    code優化

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const client = new Discord.Client();
 const auth = require('./auth.json');
 client.login(auth.key);
 client.on('ready', () => {
-    console.log(`製作者`);
-    console.log(`垃圾機器人登入到 ${client.user.tag}哈哈Gay`);
+    console.log(`製作者： Maoyue#4997`);
+    console.log(`使用時請遵守`);
+    console.log(`機器人登入到 ${client.user.tag}`);
 });
 client.on('message', msg => {
-    let set = ('983707619694882866');
+    let set = (`${auth}`);
     if (msg.channel.id === set) {
         if (msg.author.bot) return;
         if (msg.content === 'AwA') {
@@ -15,7 +16,9 @@ client.on('message', msg => {
             console.log("AwA");
         } else {
             msg.react('❌')
-            msg.channel.send(`${msg.author.tag}打錯了QwQ`)
+            msg.channel.send(`
+                    $ { msg.author.tag }
+                    打錯了AwA `)
             console.log("有人手很Gay 打錯了")
         }
     }
