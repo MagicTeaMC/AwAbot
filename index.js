bceb271fc11145e67bb7715c2bfe8226526754dc
Maoyue <95519633+MagicTeaMC@users.noreply.github.com>
2022-07-09 10:50:35 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require('./auth.json');
-client.login(auth.key);
+const config = require('./config.json');
+client.login(config.key);
 client.on('ready', () => {
     console.log(`製作者： Maoyue#4997`);
     console.log(`使用時請遵守`);
     console.log(`機器人登入到 ${client.user.tag}`);
 });
 client.on('message', msg => {
-    let set = (`${auth.AwAch}`);
+    let set = (`${config.AwAch}`);
     if (msg.channel.id === set) {
         if (msg.author.bot) return;
-        if (msg.content === 'AwA') {
+        if (msg.content === `${config.AwAword}`) {
             msg.react('✅')
-            console.log("AwA");
+            console.log(`${config.AwAword}`);
         } else {
             msg.react('❌')
-            msg.channel.send(`${msg.author.tag}打錯了AwA`)
+            msg.channel.send(`<@${msg.author.id}>打錯了AwA`)
             console.log("有人手很Gay 打錯了")
         }
     }
